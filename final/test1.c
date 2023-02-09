cf93643d1a666dafc0ce5665466bd77e53df4d05
Loizos Nic <loizosnic99@gmail.com>
2023-02-09 13:58:23 +0200

    ropExample

---
+++ final/test1.c
@@ -0,0 +1,39 @@
+// C code stored in geeks.c file
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+FILE *fptr;
+char buffer1[1000];
+
+void vulnerableFunc(char* input) {
+    char buffer[20];
+    memcpy(&buffer,input,1000);
+}
+  
+// Driver Code
+int main()
+{
+
+
+    if ((fptr = fopen("./file1.in","r")) == NULL){
+       printf("Error! opening file");
+       exit(1);
+    }
+
+    char ch;
+    int i=0;
+    /*
+    do {
+        ch = fgetc(fptr);
+        buffer1[i]=ch;
+        i++;
+    } while (ch != EOF);
+    fclose(fptr);*/
+    fread(buffer1, sizeof(char), 1000, fptr);
+    fclose(fptr);
+
+    vulnerableFunc(buffer1);
+    return 0;
+}
